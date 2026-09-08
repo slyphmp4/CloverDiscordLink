36695c055b6428e5daebbac6c625dd41e86d0c0d
./slyph <118862741+slyphmp4@users.noreply.github.com>
2026-09-09 01:30:45 +0300

    chore: update CloverDiscordLink update-check branding

--- src/main/java/dev/fread/discordbridge/update/UpdateChecker.java
+++ src/main/java/dev/fread/discordbridge/update/UpdateChecker.java
@@ -44,7 +44,7 @@ public class UpdateChecker {
                         plugin.getLogger().info("");
                         plugin.getLogger().info("=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=");
                         plugin.getLogger().info("");
-                        plugin.getLogger().info("A newer version of DiscordChatBridge is available!");
+                        plugin.getLogger().info("A newer version of CloverDiscordLink is available!");
                         plugin.getLogger().info("Latest : " + latest);
                         plugin.getLogger().info("Current: " + current);
                         plugin.getLogger().info("Download: https://github.com/slyphmp4/DiscordChatBridge/releases/latest");
